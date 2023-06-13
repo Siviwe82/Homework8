c9cd842d6c0dcb79d3286398894d8588ffb491bd
Siviwe82 <130838536+Siviwe82@users.noreply.github.com>
2023-06-13 17:00:45 +0200

    Updated script JS

--- script.js
+++ script.js
@@ -34,7 +34,7 @@ function test(response) {
   let getDay5 = new Date(response.data.daily[4].time * 1000);
   let getDay6 = new Date(response.data.daily[5].time * 1000);
 
-   const today = new Date();
+  const today = new Date();
   let days = ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"];
   let months = [
     "Jan",
@@ -54,4 +54,71 @@ function test(response) {
   date.innerHTML = `${days[getDay1.getDay()]} ${today.getDate()} ${
     months[today.getMonth()]
   }`;
-  
+  dscrp1.innerHTML = `${response.data.daily[0].condition.description} </br> wind speed: ${response.data.daily[0].wind.speed} km/hr`;
+  day1.innerHTML = days[getDay2.getDay()];
+  day2.innerHTML = days[getDay3.getDay()];
+  day3.innerHTML = days[getDay4.getDay()];
+  day4.innerHTML = days[getDay5.getDay()];
+  day5.innerHTML = days[getDay6.getDay()];
+  temp1.innerHTML = `${Math.round(response.data.daily[0].temperature.day)} °C`;
+  temp2.innerHTML = `${Math.round(
+    response.data.daily[1].temperature.maximum
+  )}° /
+    ${Math.round(response.data.daily[1].temperature.minimum)}°`;
+  temp3.innerHTML = `${Math.round(
+    response.data.daily[2].temperature.maximum
+  )}° /
+    ${Math.round(response.data.daily[2].temperature.minimum)}°`;
+  temp4.innerHTML = `${Math.round(
+    response.data.daily[3].temperature.maximum
+  )}° /
+    ${Math.round(response.data.daily[3].temperature.minimum)}°`;
+  temp5.innerHTML = `${Math.round(
+    response.data.daily[4].temperature.maximum
+  )}° /
+    ${Math.round(response.data.daily[4].temperature.minimum)}°`;
+  temp6.innerHTML = `${Math.round(
+    response.data.daily[5].temperature.maximum
+  )}° /
+    ${Math.round(response.data.daily[5].temperature.minimum)}°`;
+
+  icon1.setAttribute("src", iconUrl1);
+  icon1.setAttribute("alt", response.data.daily[0].condition.description);
+  icon2.setAttribute("src", iconUrl2);
+  icon2.setAttribute("alt", response.data.daily[1].condition.description);
+  icon3.setAttribute("src", iconUrl3);
+  icon3.setAttribute("alt", response.data.daily[2].condition.description);
+  icon4.setAttribute("src", iconUrl4);
+  icon4.setAttribute("alt", response.data.daily[3].condition.description);
+  icon5.setAttribute("src", iconUrl5);
+  icon5.setAttribute("alt", response.data.daily[4].condition.description);
+  icon6.setAttribute("src", iconUrl6);
+  icon6.setAttribute("alt", response.data.daily[5].condition.description);
+}
+
+function searchCity(city) {
+  let sheKey = "9oa4e130b348d430501cf5a6aeaaa6ft";
+  let sheUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${sheKey}&units=metric`;
+  axios.get(sheUrl).then(test);
+}
+
+function getSubmit(event) {
+  event.preventDefault();
+  let input = document.querySelector(".form-control");
+  searchCity(input.value);
+}
+
+function handlePosition(position) {
+  let lat = position.coords.latitude;
+  let lon = position.coords.longitude;
+
+  let myKey = "9oa4e130b348d430501cf5a6aeaaa6ft";
+  let geoUrl = `https://api.shecodes.io/weather/v1/forecast?lon=${lon}&lat=${lat}&key=${myKey}&units=metric`;
+
+  axios.get(geoUrl).then(test);
+
+  let form = document.querySelector("#search");
+  form.addEventListener("submit", getSubmit);
+}
+
+navigator.geolocation.getCurrentPosition(handlePosition);
