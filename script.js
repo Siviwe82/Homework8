c4c68adcb4e99ccc54675755b4a7ea2af647d570
Siviwe82 <130838536+Siviwe82@users.noreply.github.com>
2023-06-19 09:25:57 +0200

    Updated script.js

--- script.js
+++ script.js
@@ -1,99 +1,52 @@
-function test(response) {
-  let location = document.querySelector(".location");
-  let dscrp1 = document.querySelector(".dscrp1");
-  let icon1 = document.querySelector("#icon1");
-  let iconUrl1 = `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.daily[0].condition.icon}.png`;
-  let iconUrl2 = `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.daily[1].condition.icon}.png`;
-  let iconUrl3 = `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.daily[2].condition.icon}.png`;
-  let iconUrl4 = `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.daily[3].condition.icon}.png`;
-  let iconUrl5 = `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.daily[4].condition.icon}.png`;
-  let iconUrl6 = `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.daily[5].condition.icon}.png`;
-  let time1 = document.querySelector(".time1");
-  let date = document.querySelector(".date");
-  let temp1 = document.querySelector(".temp1");
-  let temp2 = document.querySelector(".temp2");
-  let temp3 = document.querySelector(".temp3");
-  let temp4 = document.querySelector(".temp4");
-  let temp5 = document.querySelector(".temp5");
-  let temp6 = document.querySelector(".temp6");
-  let icon2 = document.querySelector("#icon2");
-  let icon3 = document.querySelector("#icon3");
-  let icon4 = document.querySelector("#icon4");
-  let icon5 = document.querySelector("#icon5");
-  let icon6 = document.querySelector("#icon6");
-  let day1 = document.querySelector(".day1");
-  let day2 = document.querySelector(".day2");
-  let day3 = document.querySelector(".day3");
-  let day4 = document.querySelector(".day4");
-  let day5 = document.querySelector(".day5");
-
-  let getDay1 = new Date(response.data.daily[0].time * 1000);
-  let getDay2 = new Date(response.data.daily[1].time * 1000);
-  let getDay3 = new Date(response.data.daily[2].time * 1000);
-  let getDay4 = new Date(response.data.daily[3].time * 1000);
-  let getDay5 = new Date(response.data.daily[4].time * 1000);
-  let getDay6 = new Date(response.data.daily[5].time * 1000);
-
-  const today = new Date();
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if(hours < 10) {
+    hours = `0{hours}`;
+  }
+let minutes = date.getMinutes();
+if (minutes <10) {
+  minutes = `0{minutes}`
+}
   let days = ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"];
-  let months = [
-    "Jan",
-    "Feb",
-    "Mar",
-    "Apr",
-    "May",
-    "Jun",
-    "Jul",
-    "Sept",
-    "Oct",
-    "Nov",
-    "Dec",
-  ];
-  location.innerHTML = response.data.city;
-  time1.innerHTML = `${today.getHours()}:${today.getMinutes()}, `;
-  date.innerHTML = `${days[getDay1.getDay()]} ${today.getDate()} ${
-    months[today.getMonth()]
-  }`;
-  dscrp1.innerHTML = `${response.data.daily[0].condition.description} </br> wind speed: ${response.data.daily[0].wind.speed} km/hr`;
-  day1.innerHTML = days[getDay2.getDay()];
-  day2.innerHTML = days[getDay3.getDay()];
-  day3.innerHTML = days[getDay4.getDay()];
-  day4.innerHTML = days[getDay5.getDay()];
-  day5.innerHTML = days[getDay6.getDay()];
-  temp1.innerHTML = `${Math.round(response.data.daily[0].temperature.day)} °C`;
-  temp2.innerHTML = `${Math.round(
-    response.data.daily[1].temperature.maximum
-  )}° /
-    ${Math.round(response.data.daily[1].temperature.minimum)}°`;
-  temp3.innerHTML = `${Math.round(
-    response.data.daily[2].temperature.maximum
-  )}° /
-    ${Math.round(response.data.daily[2].temperature.minimum)}°`;
-  temp4.innerHTML = `${Math.round(
-    response.data.daily[3].temperature.maximum
-  )}° /
-    ${Math.round(response.data.daily[3].temperature.minimum)}°`;
-  temp5.innerHTML = `${Math.round(
-    response.data.daily[4].temperature.maximum
-  )}° /
-    ${Math.round(response.data.daily[4].temperature.minimum)}°`;
-  temp6.innerHTML = `${Math.round(
-    response.data.daily[5].temperature.maximum
-  )}° /
-    ${Math.round(response.data.daily[5].temperature.minimum)}°`;
 
-  icon1.setAttribute("src", iconUrl1);
-  icon1.setAttribute("alt", response.data.daily[0].condition.description);
-  icon2.setAttribute("src", iconUrl2);
-  icon2.setAttribute("alt", response.data.daily[1].condition.description);
-  icon3.setAttribute("src", iconUrl3);
-  icon3.setAttribute("alt", response.data.daily[2].condition.description);
-  icon4.setAttribute("src", iconUrl4);
-  icon4.setAttribute("alt", response.data.daily[3].condition.description);
-  icon5.setAttribute("src", iconUrl5);
-  icon5.setAttribute("alt", response.data.daily[4].condition.description);
-  icon6.setAttribute("src", iconUrl6);
-  icon6.setAttribute("alt", response.data.daily[5].condition.description);
+  let day = days[date.getDay];
+  return `${days} ${hours}:${minutes}`;
+}
+
+function formatDay(timestamp) {
+ let date = new Date(timestamp * 1000);
+ let day = date.getDay();
+ let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+ return days[day];
+}
+
+function displayForestcasy(response) {
+  let forecast = response.data.daily;
+
+let forecastElement = document.querySelector("#forecast");
+
+let forecastHTML = `<div class=row>`;
+forecast.forEach(function(forecastDay, index) {
+  if (index < 6) {
+    forecastHTML =
+    forecastHTML +
+
+<div class="col-2">
+<div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
+<img 
+src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.pnp"
+alt=""
+width=""
+/>
+<div class="weather-forecast-temperatures">
+  <span class
+</div>
+</div>
+  }
+}
+
 }
 
 function searchCity(city) {
